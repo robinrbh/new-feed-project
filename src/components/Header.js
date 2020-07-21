cdcac50627a8aac703365c1bd009cdce7b5c24fd
robinrbh <rhartog@gmail.com>
2020-07-21 17:26:51 +0200

    Added some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
         <img class="logo-img" src="https://www.nuerburgring.de/typo3temp/fl_realurl_image/nuerburgring-gesamtstrecke-copyright-01-36.jpg" />  
       </div>
       <div className="title">
-        <p>Title</p>  
+        <h1>The world of Racetracks</h1>  
       </div>
       <div className="menu">
         <p>Menu</p>  
