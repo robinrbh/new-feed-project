bb2ee74584f85697fba8060f05d919b385f1842e
rokpopov <rokpopov90@gmail.com>
2020-07-21 17:35:57 +0200

    Hamburger menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
     <header className="App-header">
       <div className="logo">
-        <img class="logo-img" src="https://www.nuerburgring.de/typo3temp/fl_realurl_image/nuerburgring-gesamtstrecke-copyright-01-36.jpg" />  
+        <img
+          class="logo-img"
+          src="https://www.nuerburgring.de/typo3temp/fl_realurl_image/nuerburgring-gesamtstrecke-copyright-01-36.jpg"
+        />
       </div>
       <div className="title">
-        <h1>The world of Racetracks</h1>  
+        <h1>The world of Racetracks</h1>
       </div>
       <div className="menu">
-        <p>Menu</p>  
+        <div class="top">
+          <a href="#" class="menu_icon">
+            <i class="material-icons">dehaze</i>
+          </a>
+        </div>
+        <p>
+          <span>&#9776;</span>
+        </p>
       </div>
     </header>
-  )
+  );
 }
