bec9ac47f5260859f0fd582f164ba0ca2362dd85
robinrbh <rhartog@gmail.com>
2020-07-21 15:25:52 +0200

    changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="App-header">
       <div className="logo">
-        <p>Logo</p>  
+        <img class="logo-img" src="https://www.nuerburgring.de/typo3temp/fl_realurl_image/nuerburgring-gesamtstrecke-copyright-01-36.jpg" />  
       </div>
       <div className="title">
         <p>Title</p>  
